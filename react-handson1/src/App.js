d80e95fad89e8145740dd6dce4244a80edafde68
hariharanpk@2703 <pkhariharan2703@gmail.com>
2022-05-12 08:54:22 +0530

    First React

---
+++ react-handson1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Classcomponents from './practice components/Classcomponents';
+import Functional from './practice components/Functional';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi</h1>
+      <Functional/>
+      <Classcomponents/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-handson1/src/practice components/Classcomponents.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Classcomponents extends Component {
+  render() {
+    return (
+      <div>Classcomponents</div>
+    )
+  }
+}
